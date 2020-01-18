10eda90a665adddb65d89b5718acc86604674bc6
yhe <ehe@mit.edu>
2020-01-17 23:09:46 -0500

    set bg color

--- client/src/components/modules/Map.js
+++ client/src/components/modules/Map.js
@@ -45,10 +45,6 @@ function TileGrid(props){
 // cannot return a canvas element because of weird DOM shit; this just puts together everything required in the map
 // write test map to render
 GameMap.propTypes = {
-    /** X-length of map, in tiles */
-    mapDimX: PropTypes.number,
-    /** Z-length of map, in tiles */
-    mapDimZ: PropTypes.number,
     /** tiles on map that contain plants, i.e. have been changed */
     tiles: PropTypes.array.tile,
 };
@@ -62,7 +58,6 @@ function MapLighting(props){
     )
 }
 function toWorldUnits(tileUnits){
-    // place in center of grid space instead of corner
     return tileUnits*tileSize;
 }
 function GameMap(props){
