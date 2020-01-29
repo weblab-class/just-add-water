528ffed3fb74937fd77882150e7680c7a55281f5
yhe <ehe@mit.edu>
2020-01-28 20:22:33 -0500

    updated controls css

--- client/src/components/modules/ControlBar.js
+++ client/src/components/modules/ControlBar.js
@@ -1,6 +1,11 @@
 import React, {Component} from "react";
 import WaterCounter from './WaterCounter';
-import {Button, ButtonGroup} from '@material-ui/core';
+import {Button, ButtonGroup, SvgIcon} from '@material-ui/core';
+import DeleteSharpIcon from '@material-ui/icons/DeleteSharp';
+import PanToolSharpIcon from '@material-ui/icons/PanToolSharp';
+import ColorizeSharpIcon from '@material-ui/icons/ColorizeSharp';
+import LocalFloristSharpIcon from '@material-ui/icons/LocalFloristSharp';
+import './ControlBar.css';
 function ControlBar(props){
     /**prop types
      * @param addNewRandom
@@ -12,12 +17,23 @@ function ControlBar(props){
 
      return(
           <div className="control-bar">
+            <ButtonGroup orientation="vertical">
+            <Button onClick={props.addNewRandom}>
+              <LocalFloristSharpIcon/>
+              Add</Button>
+            <Button onClick={props.setPickMode}>
+              <ColorizeSharpIcon/>
+              Pick</Button>
+            </ButtonGroup>
             <a className={props.inputMode == "water" ? "button-water-active":"button-water-inactive"} onClick={props.handleClickWaterButton} ></a>
             <ButtonGroup orientation="vertical">
-            <Button onClick={props.addNewRandom}>Add</Button>
-            <Button onClick={props.setPickMode}>Pick</Button>
-            <Button onClick={props.setMoveMode}>Move</Button>
-            <Button onClick={props.setDeleteMode}>Delete</Button>
+            <Button onClick={props.setMoveMode}>
+              <PanToolSharpIcon/>
+               Move
+            </Button>
+            <Button onClick={props.setDeleteMode}>
+              <DeleteSharpIcon/>
+              Delete</Button>
             {props.inputMode == "view"? <span/>:<Button onClick={props.setViewMode}>Done</Button>}
             </ButtonGroup>
           </div>
