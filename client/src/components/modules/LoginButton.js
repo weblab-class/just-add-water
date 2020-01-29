2b7ff081127f663bbbd96027bddfda01797bba79
yhe <ehe@mit.edu>
2020-01-29 00:16:04 -0500

    changed toolbar layout

--- client/src/components/modules/LoginButton.js
+++ client/src/components/modules/LoginButton.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import GoogleLogin, { GoogleLogout } from "react-google-login";
+import './LoginButton.css';
 const GOOGLE_CLIENT_ID = "165089793235-ovm7mojq6cb3advrbqmis38sqqk144jt.apps.googleusercontent.com";
 function LoginButton(props){
-    return( <>
+    return( <div className="login-button">
     {props.userId ? (
         <GoogleLogout
         clientId={GOOGLE_CLIENT_ID}
@@ -17,6 +18,6 @@ function LoginButton(props){
         onSuccess={props.handleLogin}
         onFailure={(err) => console.log(err)}
         />
-    )}</>)
+    )}</div>)
 }
 export default LoginButton;
